6d9383186c42e6af7a94f447e70c3da0a54a3f4c
ardiles <ardiles.duha@gmail.com>
2017-10-28 23:49:37 +0700

    Revert "init commit"
    This reverts commit 3a5e3fe3467cc5b3260e70422a6a9b9b1da31d0b.

--- app/src/main/java/com/example/aldoduha/ujikompetensi/alertDialog/listener/KYNInfoAlertDialogListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.aldoduha.ujikompetensi.alertDialog.listener;
-
-/**
- * Created by aldoduha on 10/9/2017.
- */
-
-public interface KYNInfoAlertDialogListener {
-    public void onOk();
-}

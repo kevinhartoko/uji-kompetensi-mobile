b02ee9aa925bf41081b274d5c297da7bfd17ac4c
kevinhartoko <kevin.hartoko@metamorf.co.id>
2017-12-21 16:54:08 +0700

    tambahin action, category, success failed code, JSONKey untuk API

--- app/src/main/java/com/example/aldoduha/ujikompetensi/utility/KYNIntentConstant.java
+++ app/src/main/java/com/example/aldoduha/ujikompetensi/utility/KYNIntentConstant.java
@@ -25,9 +25,39 @@ public class KYNIntentConstant {
 
     //action
     public static final String ACTION_LOGIN = "login";
+    public static final String ACTION_LOGOUT = "logout";
+    public static final String ACTION_USER_DETAIL = "user_detail";
+    public static final String ACTION_INTERVIEWEE_DETAIL = "interviewee_detail";
+    public static final String ACTION_QUESTION_DETAIL = "question_detail";
+    public static final String ACTION_TEMPLATE_DETAIL = "template_detail";
+    public static final String ACTION_USER_LIST = "user_list";
+    public static final String ACTION_QUESTION_LIST = "question_list";
+    public static final String ACTION_INTERVIEWEE_LIST = "interviewee_list";
+    public static final String ACTION_TEMPLATE_LIST = "template_list";
+    public static final String ACTION_SUBMIT_INTERVIEWEE_DATA = "submit_interviewee_data";
+    public static final String ACTION_SUBMIT_QUESTION = "submit_question";
+    public static final String ACTION_SUBMIT_USER = "submit_user";
+    public static final String ACTION_SUBMIT_TEMPLATE = "submit_template";
+    public static final String ACTION_SUBMIT_FEEDBACK = "submit_feedback";
+    public static final String ACTION_GENERATE_QEUSTION = "generate_question";
 
     //category
-    public static final String CATEGORY_LOGIN = "login";
+    public static final String CATEGORY_LOGIN = "category_login";
+    public static final String CATEGORY_LOGOUT = "category_logout";
+    public static final String CATEGORY_USER_DETAIL = "category_user_detail";
+    public static final String CATEGORY_INTERVIEWEE_DETAIL = "category_interviewee_detail";
+    public static final String CATEGORY_QUESTION_DETAIL = "category_question_detail";
+    public static final String CATEGORY_TEMPLATE_DETAIL = "category_template_detail";
+    public static final String CATEGORY_USER_LIST = "category_user_list";
+    public static final String CATEGORY_QUESTION_LIST = "category_question_list";
+    public static final String CATEGORY_INTERVIEWEE_LIST = "category_interviewee_list";
+    public static final String CATEGORY_TEMPLATE_LIST = "category_template_list";
+    public static final String CATEGORY_SUBMIT_INTERVIEWEE_DATA = "category_submit_interviewee_data";
+    public static final String CATEGORY_SUBMIT_QUESTION = "category_submit_question";
+    public static final String CATEGORY_SUBMIT_USER = "category_submit_user";
+    public static final String CATEGORY_SUBMIT_TEMPLATE = "category_submit_template";
+    public static final String CATEGORY_SUBMIT_FEEDBACK = "category_submit_feedback";
+    public static final String CATEGORY_GENERATE_QEUSTION = "category_generate_question";
 
     public static final int CODE_FAILED = 0;
     public static final int CODE_FAILED_TOKEN = 215;
@@ -45,4 +75,34 @@ public class KYNIntentConstant {
     //code for api
     public static final int CODE_LOGIN_SUCCESS = 300;
     public static final int CODE_LOGIN_FAILED = 301;
+    public static final int CODE_LOGOUT_SUCCESS = 302;
+    public static final int CODE_LOGOUT_FAILED = 303;
+    public static final int CODE_USER_DETAIL_SUCCESS = 304;
+    public static final int CODE_USER_DETAIL_FAILED = 305;
+    public static final int CODE_INTERIVEWEE_DETAIL_SUCCESS = 306;
+    public static final int CODE_INTERIVEWEE_DETAIL_FAILED = 307;
+    public static final int CODE_QUESTION_DETAIL_SUCCESS = 308;
+    public static final int CODE_QUESTION_DETAIL_FAILED = 309;
+    public static final int CODE_TEMPLATE_DETAIL_SUCCESS = 310;
+    public static final int CODE_TEMPLATE_DETAIL_FAILED = 311;
+    public static final int CODE_USER_LIST_SUCCESS = 312;
+    public static final int CODE_USER_LIST_FAILED = 313;
+    public static final int CODE_QUESTION_LIST_SUCCESS = 314;
+    public static final int CODE_QUESTION_LIST_FAILED = 315;
+    public static final int CODE_TEMPLATE_LIST_SUCCESS = 316;
+    public static final int CODE_TEMPLATE_LIST_FAILED = 317;
+    public static final int CODE_INTERVIEWEE_LIST_SUCCESS = 318;
+    public static final int CODE_INTERVIEWEE_LIST_FAILED = 319;
+    public static final int CODE_SUBMIT_INTERVIEWEE_DATA_SUCCESS = 320;
+    public static final int CODE_SUBMIT_INTERVIEWEE_DATA_FAILED = 321;
+    public static final int CODE_SUBMIT_QUESTION_SUCCESS = 322;
+    public static final int CODE_SUBMIT_QUESTION_FAILED = 323;
+    public static final int CODE_SUBMIT_USER_SUCCESS = 324;
+    public static final int CODE_SUBMIT_USER_FAILED = 325;
+    public static final int CODE_SUBMIT_TEMPLATE_SUCCESS = 326;
+    public static final int CODE_SUBMIT_TEMPLATE_FAILED = 327;
+    public static final int CODE_SUBMIT_FEEDBACK_SUCCESS = 328;
+    public static final int CODE_SUBMIT_FEEDBACK_FAILED = 329;
+    public static final int CODE_GENERATE_QUESTION_SUCCESS = 330;
+    public static final int CODE_GENERATE_QUESTION_FAILED = 331;
 }
--- app/src/main/java/com/example/aldoduha/ujikompetensi/utility/KYNJSONKey.java
+++ app/src/main/java/com/example/aldoduha/ujikompetensi/utility/KYNJSONKey.java
@@ -17,4 +17,40 @@ public class KYNJSONKey {
     public static String VAL_SUCCESS = "success";
     public static String VAL_ERROR = "error";
     public static String VAL_MESSAGE_FAILED = "Permintaan Gagal";
+
+    //answer
+    public static String KEY_ANSWER_ANSWER = "answer";
+    public static String KEY_ANSWER_IS_TRUE = "is_true";
+    public static String KEY_ANSWER_FK_INTERVIEWEE = "interviewee";
+    public static String KEY_ANSWER_FK_QUESTION = "question";
+
+    //feedback
+    public static String KEY_FEEDBACK_FEEDBACK = "feedback";
+    public static String KEY_FK_USER_FEEDBACK = "user";
+    public static String KEY_FEEDBACK_FK_INTERVIEWEE = "interviewee";
+
+    //question
+    public static String KEY_QUESTION_DESCRIPTION = "description";
+    public static String KEY_QUESTION_ANSWER_A = "answer_a";
+    public static String KEY_QUESTION_ANSWER_B = "answer_b";
+    public static String KEY_QUESTION_ANSWER_C = "answer_c";
+    public static String KEY_QUESTION_ANSWER_D = "answer_d";
+    public static String KEY_QUESTION_TRUE_ANSWER = "true_answer";
+    public static String KEY_QUESTION_BOBOT = "bobot";
+    public static String KEY_QUESTION_CODE = "code";
+
+    //template
+    public static String KEY_TEMPLATE_DESCRIPTION = "description";
+    public static String KEY_TEMPLATE_TOTAL_QUESTION = "total_question";
+
+    //template question
+    public static String KEY_TEMPLATE_QUESITON_BANYAK_SOAL = "banyak_soal";
+    public static String KEY_TEMPLATE_QUESTION_BOBOT = "bobot";
+    public static String KEY_TEMPLATE_QUESTION_FK_TEMPLATE = "template";
+
+    //user
+    public static String KEY_USER_USERNAME = "username";
+    public static String KEY_USER_NAME = "name";
+    public static String KEY_USER_PASSWORD = "password";
+    public static String KEY_USER_ROLE = "role";
 }
